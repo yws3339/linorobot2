e035231baddd55a8bfc87e36a217439d4590f442
Juan Jimeno <jimenojmm@gmail.com>
2024-07-22 14:52:02 +0000

    improved generated world by using occuped_thresh and free_thresh instead of using contours

--- linorobot2_gazebo/scripts/map2gazebo.py
+++ linorobot2_gazebo/scripts/map2gazebo.py
@@ -241,9 +241,8 @@ XML_SDF_TEMPLATE = """
 """
 
 
-class MapConverter():
-    def __init__(self, map_dir, export_dir, world_dir, height=1.0):
-        
+class MapConverter:
+    def __init__(self, map_dir, export_dir, world_dir, height=1.5):
         self.height = height
         self.export_dir = export_dir
         self.world_dir = world_dir
@@ -252,142 +251,125 @@ class MapConverter():
     def map_callback(self):
         all_maps = self._extract_maps(self.map_dir)
         for key, value in all_maps.items():
-          map_file_dir = value[0] if ".pgm" in value[0] or ".png" in value[0] else value[1]
-          info_dir = value[0] if ".yaml" in value[0] else value[1]
-          map_array = cv2.imread(map_file_dir)
-          map_array = cv2.flip(map_array, 0)
-          print(f'loading map file: {map_file_dir}')
-          try:
-              map_array = cv2.cvtColor(map_array, cv2.COLOR_BGR2GRAY)
-          except cv2.error as err:
-              print(err, "Conversion failed: Invalid image input, please check your file path")    
-              sys.exit()
+            map_file_dir = value[0] if ".pgm" in value[0] or ".png" in value[0] else value[1]
+            info_dir = value[0] if ".yaml" in value[0] else value[1]
+            map_array = cv2.imread(map_file_dir)
+            map_array = cv2.flip(map_array, 0)
+            print(f'loading map file: {map_file_dir}')
+            try:
+                map_array = cv2.cvtColor(map_array, cv2.COLOR_BGR2GRAY)
+            except cv2.error as err:
+                print(err, "Conversion failed: Invalid image input, please check your file path")    
+                sys.exit()
 
-          with open(info_dir, 'r') as stream:
-              map_info = yaml.load(stream, Loader=yaml.FullLoader) 
-          
-          # set all -1 (unknown) values to 0 (unoccupied)
-          map_array[map_array < 0] = 0
-          contours = self.get_occupied_regions(map_array)
-          print('Processing...')
-          meshes = [self.contour_to_mesh(c, map_info) for c in contours]
-
-          corners = list(np.vstack(contours))
-          corners = [c[0] for c in corners]
-          mesh = trimesh.util.concatenate(meshes)
-
-          if not self.export_dir.endswith('/'):
-              self.export_dir = self.export_dir + '/'
-
-          if not self.world_dir.endswith('/'):
-              self.world_dir = self.world_dir + '/'
+            with open(info_dir, 'r') as stream:
+                map_info = yaml.safe_load(stream)
             
-          if not os.path.exists(self.export_dir + f'{key}/meshes/'):
-            os.makedirs(self.export_dir + f'{key}/meshes/')
+            # set all -1 (unknown) values to 255 (white/unoccupied)
+            map_array[map_array < 0] = 255
+            
+            print('Processing...')
+            mesh = self.create_mesh_from_map(map_array, map_info)
 
-          if not os.path.exists(self.world_dir):
-            os.makedirs(self.world_dir)
+            if not self.export_dir.endswith('/'):
+                self.export_dir = self.export_dir + '/'
 
-          stl_dir = self.export_dir + f'{key}/meshes/' + f'{key}.stl'
-          sdf_dir = self.export_dir + f'{key}/' + f'{key}.sdf'
-          config_dir = self.export_dir + f'{key}/model.config'
+            if not self.world_dir.endswith('/'):
+                self.world_dir = self.world_dir + '/'
+            
+            if not os.path.exists(self.export_dir + f'{key}/meshes/'):
+                os.makedirs(self.export_dir + f'{key}/meshes/')
 
-          sdf_data = XML_SDF_TEMPLATE.format(name=key)
-          config_data = XML_MODEL_CONFIG_TEMPLATE.format(name=key)
-          print(f'export file: {stl_dir}')
-          
-          with open(stl_dir, 'wb') as f:
-              mesh.export(f, "stl")
+            if not os.path.exists(self.world_dir):
+                os.makedirs(self.world_dir)
 
-          with open(sdf_dir, 'w') as f:
-              f.write(sdf_data)
+            stl_dir = self.export_dir + f'{key}/meshes/' + f'{key}.stl'
+            sdf_dir = self.export_dir + f'{key}/' + f'{key}.sdf'
+            config_dir = self.export_dir + f'{key}/model.config'
 
-          with open(config_dir, 'w') as f:
-              f.write(config_data)
+            sdf_data = XML_SDF_TEMPLATE.format(name=key)
+            config_data = XML_MODEL_CONFIG_TEMPLATE.format(name=key)
+            print(f'export file: {stl_dir}')
+            
+            with open(stl_dir, 'wb') as f:
+                mesh.export(f, "stl")
 
-          # create world file create new parameter to save this files.
-          world_data = XML_WORLD_TEMPLATE.format(name=key)
-          world_dir = self.world_dir + f'{key}.world'
-          with open(world_dir, 'w') as f:
-              f.write(world_data)
+            with open(sdf_dir, 'w') as f:
+                f.write(sdf_data)
+
+            with open(config_dir, 'w') as f:
+                f.write(config_data)
+
+            # create world file
+            world_data = XML_WORLD_TEMPLATE.format(name=key)
+
+            world_dir = self.world_dir + f'{key}.world'
+            with open(world_dir, 'w') as f:
+                f.write(world_data)
 
     def _extract_maps(self, directory_path):
-      files_dict = {}
+        files_dict = {}
 
-      for filename in os.listdir(directory_path):
-          base_name, extension = os.path.splitext(filename)
-          
-          if extension in ['.pgm', '.yaml', '.png']:
-              if base_name in files_dict:
-                  files_dict[base_name].append(os.path.join(directory_path, filename))
-              else:
-                  files_dict[base_name] = [os.path.join(directory_path, filename)]
-      return files_dict
-    
-    def get_occupied_regions(self, map_array):
-        """
-        Get occupied regions of map
-        """
-        map_array = map_array.astype(np.uint8)
-        _, thresh_map = cv2.threshold(
-                map_array, 253, 255, cv2.THRESH_BINARY)
-        contours, hierarchy = cv2.findContours(
-                thresh_map, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
-        # Using cv2.RETR_CCOMP classifies external contours at top level of
-        # hierarchy and interior contours at second level.  
-        # If the whole space is enclosed by walls RETR_EXTERNAL will exclude
-        # all interior obstacles e.g. furniture.
-        # https://docs.opencv.org/trunk/d9/d8b/tutorial_py_contours_hierarchy.html
-        hierarchy = hierarchy[0]
-        output_contours = []
-        for idx, contour in enumerate(contours):
-            output_contours.append(contour) if 0 not in contour else print('Remove image boundary')
+        for filename in os.listdir(directory_path):
+            base_name, extension = os.path.splitext(filename)
             
-        return output_contours
+            if extension in ['.pgm', '.yaml', '.png']:
+                if base_name in files_dict:
+                    files_dict[base_name].append(os.path.join(directory_path, filename))
+                else:
+                    files_dict[base_name] = [os.path.join(directory_path, filename)]
+        
+        return files_dict
 
-    def contour_to_mesh(self, contour, metadata):
+    def create_mesh_from_map(self, map_array, metadata):
         height = np.array([0, 0, self.height])
-        meshes = []
-        for point in contour:
-            x, y = point[0]
-            vertices = []
-            new_vertices = [
-                    self.coords_to_loc((x, y), metadata),
-                    self.coords_to_loc((x, y+1), metadata),
-                    self.coords_to_loc((x+1, y), metadata),
-                    self.coords_to_loc((x+1, y+1), metadata)]
-            vertices.extend(new_vertices)
-            vertices.extend([v + height for v in new_vertices])
-            faces = [[0, 2, 4],
-                     [4, 2, 6],
-                     [1, 2, 0],
-                     [3, 2, 1],
-                     [5, 0, 4],
-                     [1, 0, 5],
-                     [3, 7, 2],
-                     [7, 6, 2],
-                     [7, 4, 6],
-                     [5, 4, 7],
-                     [1, 5, 3],
-                     [7, 3, 5]]
-            mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
-            if not mesh.is_volume:
-                mesh.fix_normals()
-            meshes.append(mesh)
-        mesh = trimesh.util.concatenate(meshes)
-        # mesh.remove_duplicate_faces()
-        mesh.update_faces(mesh.unique_faces())
-        # mesh will still have internal faces.  Would be better to get
-        # all duplicate faces and remove both of them, since duplicate faces
-        # are guaranteed to be internal faces
+        vertices = []
+        faces = []
+        vertex_count = 0
+
+        thresh_map = map_array.copy()
+
+        # Apply the thresholds
+        thresh_map[map_array >= metadata["occupied_thresh"] * 255] = 255  # Occupied cells
+        thresh_map[map_array <= metadata["free_thresh"] * 255] = 0  # Free cells
+        thresh_map[(map_array > metadata["free_thresh"] * 255) & (map_array < metadata["occupied_thresh"] * 255)] = 127  # Unknown cells
+
+        # Reduce resolution to simplify the mesh
+        step = 1  # Adjust this value to change the simplification level
+
+        for y in range(0, thresh_map.shape[0] - 1, step):
+            for x in range(0, thresh_map.shape[1] - 1, step):
+                if thresh_map[y, x] == 0:  # If the pixel is black (occupied)
+                    new_vertices = [
+                        self.coords_to_loc((x, y), metadata),
+                        self.coords_to_loc((x, y+step), metadata),
+                        self.coords_to_loc((x+step, y), metadata),
+                        self.coords_to_loc((x+step, y+step), metadata)
+                    ]
+                    vertices.extend(new_vertices)
+                    vertices.extend([v + height for v in new_vertices])
+
+                    new_faces = [
+                        [vertex_count + i for i in face]
+                        for face in [[0, 2, 4], [4, 2, 6], [1, 2, 0], [3, 2, 1],
+                                     [5, 0, 4], [1, 0, 5], [3, 7, 2], [7, 6, 2],
+                                     [7, 4, 6], [5, 4, 7], [1, 5, 3], [7, 3, 5]]
+                    ]
+                    faces.extend(new_faces)
+                    vertex_count += 8
+
+        mesh = trimesh.Trimesh(vertices=vertices, faces=faces)
+        if not mesh.is_volume:
+            mesh.fix_normals()
+        
+        mesh.remove_duplicate_faces()
+        
         return mesh
 
-    def coords_to_loc(self,coords, metadata):
+    def coords_to_loc(self, coords, metadata):
         x, y = coords
         loc_x = x * metadata['resolution'] + metadata['origin'][0]
         loc_y = y * metadata['resolution'] + metadata['origin'][1]
-        # TODO: transform (x*res, y*res, 0.0) by Pose map_metadata.origin
-        # instead of assuming origin is at z=0 with no rotation wrt map frame
         return np.array([loc_x, loc_y, 0.0])
 
 if __name__ == "__main__":
